4b81e08b0a42a1c5c2545d5cc39222bcfd71d3e7
Mishchenko Anton (BhP/HRL3.2-FI) <mia4bh@bosch.com>
2026-02-05 14:32:48 +0100

    2

---
+++ 3_javascript_basic/1_variable/script.js
@@ -0,0 +1,32 @@
+/* Data Types:
+undefined, null, boolean, string, symbol, number, object
+*/
+
+var myName = "Anton"
+
+myname = 8
+
+let ourName = "freeCodeCamp"
+
+const pi = 3.14
+
+var a;
+var b = 2;
+
+a = 7;
+
+b = a;
+
+b++;
+b++;
+
+var remainder;
+remainder  = 11 % 3;
+
+a += 6 // gleich wie a = a + 6
+b -= 7 // gleich wie b = 7 - b
+b *= 2 // gleich wie b = 2 * b
+a /= 3 // gleich wie a = a / 3
+
+var myStr = "I am a \" double quoted\" string inside \" double qoutes\""
+//wie in Java
